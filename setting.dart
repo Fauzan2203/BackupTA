6eb149d27376a7370b6728417a80b2a22c2dcdb5
Fauzan2203 <85207071+Fauzan2203@users.noreply.github.com>
2024-06-28 20:53:09 +0700

    Add files via upload

---
+++ setting.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Setting extends StatefulWidget {
+  const Setting({Key? key}) : super(key: key);
+
+  @override
+  _SettingState createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(178, 209, 238, 1),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black),
+        backgroundColor: const Color.fromRGBO(255, 255, 255, 255),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: const Text(
+          "Setting",
+          style: TextStyle(fontFamily: "Raleway"),
+        ),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            FirebaseAuth.instance.signOut();
+            Navigator.pushNamed(context, "/");
+          },
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(double.infinity, 48),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+          ),
+          child: Text(
+            'Log Out',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+}
