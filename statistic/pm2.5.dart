77bdc82b841a6b7cff2f40f25125368c459f839c
Fauzan2203 <85207071+Fauzan2203@users.noreply.github.com>
2024-07-03 14:17:00 +0700

    Update pm2.5.dart

--- statistic/pm2.5.dart
+++ statistic/pm2.5.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
+import 'chart_data_provider.dart';
 
 class PM25 extends StatefulWidget {
   const PM25({Key? key}) : super(key: key);
@@ -10,10 +12,8 @@ class PM25 extends StatefulWidget {
   _PM25State createState() => _PM25State();
 }
 
-class _PM25State extends State<PM25> {
-  List<DataPoint> chartData = [];
+class _PM25State extends State<PM25> with AutomaticKeepAliveClientMixin {
   Timer? _timer;
-  double globalCurrentSensorValue = 0;
 
   @override
   void dispose() {
@@ -24,36 +24,38 @@ class _PM25State extends State<PM25> {
   @override
   void initState() {
     super.initState();
-    _timer = Timer.periodic(const Duration(minutes: 2), _generateTrace);
+    _timer = Timer.periodic(const Duration(seconds: 5), _generateTrace);
   }
 
   _generateTrace(Timer t) {
     if (mounted) {
-      setState(() {
-        chartData.add(DataPoint(DateTime.now(), globalCurrentSensorValue));
-      });
+      var provider = Provider.of<ChartDataProvider>(context, listen: false);
+      provider.addDataPoint(
+          DataPoint(DateTime.now(), provider.globalCurrentSensorValue));
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context); // Ensure AutomaticKeepAliveClientMixin works
     return Scaffold(
       backgroundColor: const Color.fromRGBO(178, 209, 238, 1),
       body: Container(
         alignment: Alignment.topCenter,
-        height: 600,
-        margin: EdgeInsets.only(left: 15, right: 15),
-        padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+        height: 590,
+        margin: const EdgeInsets.only(left: 15, right: 15, top: 15),
+        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
         decoration: const BoxDecoration(
           color: Colors.white,
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(50),
-              bottomRight: Radius.circular(50)),
+          borderRadius: BorderRadius.all(Radius.circular(50)),
         ),
         child: buildStreamBuilder(),
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 
   Widget buildStreamBuilder() {
     return StreamBuilder(
@@ -66,27 +68,32 @@ class _PM25State extends State<PM25> {
             ),
           );
         } else {
+          var provider = Provider.of<ChartDataProvider>(context, listen: false);
           var documents = snapshot.data?.docs ?? [];
           for (var f in documents) {
             if (f.id == 'DHT11') {
-              print('current value = ${f['PM25']}');
-              globalCurrentSensorValue = double.parse(f['PM25']);
+              print('current PM = ${f['PM25']}');
+              provider.setGlobalCurrentSensorValue(double.parse(f['PM25']));
             }
           }
 
           return Center(
-            child: SfCartesianChart(
-              primaryXAxis: DateTimeAxis(),
-              primaryYAxis:
-                  NumericAxis(minimum: 0, maximum: 260, opposedPosition: true),
-              series: <ChartSeries>[
-                LineSeries<DataPoint, DateTime>(
-                  dataSource: chartData,
-                  xValueMapper: (DataPoint data, _) => data.time,
-                  yValueMapper: (DataPoint data, _) => data.value,
-                  width: 5,
-                ),
-              ],
+            child: Consumer<ChartDataProvider>(
+              builder: (context, chartDataProvider, child) {
+                return SfCartesianChart(
+                  primaryXAxis: DateTimeAxis(),
+                  primaryYAxis: NumericAxis(
+                      minimum: 0, maximum: 260, opposedPosition: true),
+                  series: <ChartSeries>[
+                    LineSeries<DataPoint, DateTime>(
+                      dataSource: chartDataProvider.chartData,
+                      xValueMapper: (DataPoint data, _) => data.time,
+                      yValueMapper: (DataPoint data, _) => data.value,
+                      width: 5,
+                    ),
+                  ],
+                );
+              },
             ),
           );
         }
